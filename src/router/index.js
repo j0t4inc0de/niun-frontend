80ef045815d4a031da1d893840285e4760c257a8
Juan <juan.erices04@inacapmail.cl>
2026-01-11 17:35:26 -0300

    configuracion de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/auth/LoginView.vue'
+import { useAuthStore } from '../stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/auth/RegisterView.vue'),
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: () => import('../views/dashboard/HomeView.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/',
+      redirect: '/login', // Por ahor la home redirige al login
+    },
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else if ((to.path === '/login' || to.path === '/register') && authStore.isAuthenticated) {
+    next('/dashboard')
+  } else {
+    next()
+  }
 })
 
 export default router
