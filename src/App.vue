78525eac9809d9a25416eaff88bedd56d8150262
Juan <juan.erices04@inacapmail.cl>
2026-01-11 16:57:25 -0300

    test tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 <template>
   <div class="bg-red-500 text-white p-4 text-center">
     Si esto se ve rojo, Tailwind funciona correctamente
+    <div><span>Hola Mundo!</span></div>
   </div>
 </template>
 
