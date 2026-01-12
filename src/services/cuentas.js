a4589d3991f7b215286948d4825ba42b315e1d5d
Juan <juan.erices04@inacapmail.cl>
2026-01-12 13:32:14 -0300

    Servicio para crud de cuentas

---
+++ src/services/cuentas.js
@@ -0,0 +1,20 @@
+// src/services/cuentas.js
+import http from './api/http'
+
+export default {
+  /**
+   * Obtiene todas las cuentas del usuario.
+   * Método: GET
+   */
+  obtenerTodas() {
+    return http.get('/cuentas/')
+  },
+
+  crear(datosCuenta) {
+    return http.post('/cuentas/', datosCuenta)
+  },
+
+  eliminar(id) {
+    return http.delete(`/cuentas/${id}/`)
+  },
+}
