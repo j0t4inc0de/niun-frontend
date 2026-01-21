ef854136c388673eeb381d22f9337be78b394bac
Juan <juan.erices04@inacapmail.cl>
2026-01-21 00:21:07 -0300

    servicio para la api de archivos

---
+++ src/services/files.js
@@ -0,0 +1,26 @@
+// src/services/files.js
+import http from './api/http'
+
+export default {
+  listar() {
+    return http.get('/files/')
+  },
+
+  subir(formData) {
+    return http.post('/files/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+  },
+
+  descargar(id) {
+    return http.get(`/files/${id}/download/`, {
+      responseType: 'blob',
+    })
+  },
+
+  eliminar(id) {
+    return http.delete(`/files/${id}/`)
+  },
+}
