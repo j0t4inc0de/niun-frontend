2a993541329fa98a7f4480cf5414e966a59c193d
Jota Linux <jericesb5@gmail.com>
2026-02-02 18:47:36 -0300

    comentario a seccion

--- src/views/dashboard/HomeView.vue
+++ src/views/dashboard/HomeView.vue
@@ -35,7 +35,7 @@ onMounted(() => {
                 </p>
             </div>
         </div>
-
+        <!-- Tarjeta de PLan -->
         <div class="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8">
 
             <div
@@ -124,7 +124,7 @@ onMounted(() => {
                             <div class="flex items-center gap-1.5 text-gray-600 dark:text-mako-300">
                                 <span>Max: <span class="text-gray-900 dark:text-white font-bold">{{
                                     auth.user?.limites?.cuentas?.total ||
-                                    10 }}</span></span>
+                                        10 }}</span></span>
                             </div>
                         </div>
                     </div>
